50f26b4cbabdbc52ce95e8637e0252cef6fb4686
Ray <raymund.lew@gmail.com>
2014-07-17 11:41:54 -0700

    added some routes

--- shared/routes.js
+++ shared/routes.js
@@ -1,12 +1,22 @@
-Router.configure({
-  notFoundTemplate: 'home'
-});
-
 Router.map(function() {
-  this.route('home', {
-    path: '/'
+  this.route('settings', {
+    path: /\/settings|\/config|\/configure|\/setup|\/set-up/
   });
   this.route('player', {
     path: '/player'
+  });
+  // home template will be returned for all routes not above
+  this.route('home', {
+    path: '*',
+    onRun: function() {
+      // if the player hasn't been set up yet, go to settings page
+      if (!Settings.findOne()) {
+        Router.go('/settings');
+      }
+      // if there is a user logged in already, go to the player
+      if (Meteor.userId()) {
+        Router.go('/player');
+      }
+    }
   });
 });
