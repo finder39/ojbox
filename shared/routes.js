8e2441c165916db04306c01cdf8a0da18c672581
18thletter <raymund.lew@gmail.com>
2014-10-14 14:21:10 -0700

    Updated Meteor to 0.9.4, and redid routes.
    Updated Meteor to 0.9.4, and corresponding packages.
    
    Redid routes in this way:
    - No "catch all" route
    - Login routes to player if user is logged in
    - Player routes to login if user is not logged in

--- shared/routes.js
+++ shared/routes.js
@@ -1,13 +1,33 @@
-Router.map(function() {
-  // this template will be returned for all routes
-  this.route('*', {
-    template: 'home',
-    waitOn: function() {
-      return [
-        Meteor.subscribe("currentSong"),
-        Meteor.subscribe("playlist")
-      ];
-    },
-    fastRender: true,
-  });
+Router.route('login', {
+  onBeforeAction: function() {
+    if (Meteor.userId()) {
+      this.redirect('player');
+    }
+  },
+  fastRender: true,
 });
+Router.route('player', {
+  template: 'playerPage',
+  waitOn: function() {
+    return [
+      Meteor.subscribe("currentSong"),
+      Meteor.subscribe("playlist")
+    ];
+  },
+  onBeforeAction: function() {
+    if (!Meteor.userId()) {
+      this.redirect('login');
+    }
+  },
+  fastRender: true,
+});
+Router.route('/', {
+  template: 'home',
+  onBeforeAction: function() {
+    if (!Meteor.userId()) {
+      this.redirect('login');
+    } else {
+      this.redirect('player');
+    }
+  }
+})
