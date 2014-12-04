8ddbed50974da4485b44db6ae2888e0765caaa5f
18thletter <raymund.lew@gmail.com>
2014-12-03 21:22:29 -0800

    added Kadira keys. will move this to settings file later

--- collections/boxes.js
+++ collections/boxes.js
@@ -2,7 +2,7 @@ Boxes = new Mongo.Collection("boxes");
 
 // don't allow changes to the box from the client (at least not yet)
 // the only attribute of a box currently is it's name, which should
-// never be mondified
+// never be modified
 Boxes.allow({
   'insert': function(userId, doc) {
     return false;
---
+++ server/kadira.js
@@ -0,0 +1,1 @@
+Kadira.connect('YsuuDpnkvBJKTzgHS', '25e09ea0-ed3c-4226-882d-8f36196b5143');
